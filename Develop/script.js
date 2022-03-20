ca054921d7dc5e2dfe0b41c0723787c3a52f1a40
Shogun <shogunzaku@gmail.com>
2022-03-20 15:32:23 -0700

    added prompts

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,12 @@
 // Assignment Code
+// added prompts
 var generateBtn = document.querySelector("#generate");
+let userChoice = window.prompt("How Many Characters would you like your password to contain?");
+let userChoice2 = window.prompt("Include lowercase?");
+let userChoice3 = window.prompt("Include uppercase?");
+let userChoice4 = window.prompt("Include numeric characters");
+let userChoice5 = window.prompt("Include special characters?");
+
 
 console.log(passNames);
 
